99fc3892b512f6d2e1b6ad5815b38eb5247cf440
Иван Гайдуков <ivan.gaydukov2005@gmail.com>
2024-11-06 01:57:29 +0300

    added DbContext

---
+++ Store.Application/Interfaces/IDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Domain;
+namespace Store.Application.Interfaces;
+
+internal interface IDbContext
+{
+    DbSet<Product> Products { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
