ffaa057dabc1da8dc78cd0aa40e8d4afcecd28a5
Иван Гайдуков <ivan.gaydukov2005@gmail.com>
2024-11-06 01:47:39 +0300

    Добавьте файлы проекта.

---
+++ Store.Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Domain;
+
+public class Product
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string UrlImage { get; set; }
+    public required string Article { get; set; }
+}
